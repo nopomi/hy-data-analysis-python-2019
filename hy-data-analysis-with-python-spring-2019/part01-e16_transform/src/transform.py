f8860120b693cc7fb356a83548c0000755567b5f
Miska Noponen <miska.noponen@gmail.com>
2019-05-26 14:53:56 +0300

    completex part 1 exercise 16

--- hy-data-analysis-with-python-spring-2019/part01-e16_transform/src/transform.py
+++ hy-data-analysis-with-python-spring-2019/part01-e16_transform/src/transform.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
-        
+    #convert to list of integers
+    s1_int = map(int, s1.split())
+    s2_int = map(int, s2.split())
+    #multiplicate elements and add to one list
+    zipped = zip(s1_int, s2_int)
+    L = []
+    for i in zipped:
+        L.append(i[0]*i[1])
+    return L
+
 def main():
+    print(transform("1 5 3", "2 6 -1"))
 
 if __name__ == "__main__":
     main()
