510014c1d75736af9dadfb664d39c5bd80ff5ba0
Miska Noponen <miska.noponen@gmail.com>
2019-05-26 14:57:24 +0300

    completex part 1 exercise 17

--- hy-data-analysis-with-python-spring-2019/part01-e17_positive_list/src/positive_list.py
+++ hy-data-analysis-with-python-spring-2019/part01-e17_positive_list/src/positive_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 def positive_list(L):
-    return []
-    
+    return list(filter(is_positive, L))
+
+def is_positive(x):
+    return x > 0
+
 def main():
-    
+    print(positive_list([2,-2,0,1,-7]))
+
 if __name__ == "__main__":
     main()
