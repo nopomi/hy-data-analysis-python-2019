d027416cd268801d5e6516e40c6b7ecca8260681
Miska Noponen <miska.noponen@gmail.com>
2019-05-26 14:33:32 +0300

    completed part 1 exercises up to 15

--- hy-data-analysis-with-python-spring-2019/part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ hy-data-analysis-with-python-spring-2019/part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+    reversed = {}
+    for key in d.keys():
+        for fin in d[key]:
+            if fin in reversed.keys():
+                reversed[fin].append(key)
+            else:
+                reversed[fin] = [key]
+    return reversed
 
 def main():
+    d={'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    print(reverse_dictionary(d))
 
 if __name__ == "__main__":
     main()
--- hy-data-analysis-with-python-spring-2019/part01-e14_find_matching/src/find_matching.py
+++ hy-data-analysis-with-python-spring-2019/part01-e14_find_matching/src/find_matching.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    list = []
+    for i, w in enumerate(L):
+        if(pattern in w): list.append(i)
+    return list
 
 def main():
+    print(find_matching(["sensitive", "engine", "rubbish", "comment"], "en"))
 
 if __name__ == "__main__":
     main()
