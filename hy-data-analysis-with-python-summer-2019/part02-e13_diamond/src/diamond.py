85a4c94569653e5dd73202ebec33666ec2d376a0
Miska Noponen <miska.noponen@gmail.com>
2020-01-27 22:37:46 +0200

    completed exercise 13 of week 2

--- hy-data-analysis-with-python-summer-2019/part02-e13_diamond/src/diamond.py
+++ hy-data-analysis-with-python-summer-2019/part02-e13_diamond/src/diamond.py
@@ -3,10 +3,18 @@
 import numpy as np
 
 def diamond(n):
-    return np.array([])
+    top_right = np.eye(n, dtype=int)
+    top_left = np.rot90(top_right)
+    top_half = np.concatenate((top_left, top_right[:,1:]), axis=1)
+    full_diamond = np.concatenate((top_half,np.flip(top_half)[1:]),axis=0)
+    return np.array(full_diamond)
 
 def main():
-    pass
+    print(diamond(1))
+    print(diamond(2))
+    print(diamond(3))
+    print(diamond(4))
+    print(diamond(10))
 
 if __name__ == "__main__":
     main()
