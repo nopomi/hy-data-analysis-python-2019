85dfb1ac0d9536fe03eb2c7908bf4e96a207f0b7
Miska Noponen <miska.noponen@gmail.com>
2020-01-27 22:47:48 +0200

    completed exercise 14 of week 2

--- hy-data-analysis-with-python-summer-2019/part02-e14_vector_lengths/src/vector_lengths.py
+++ hy-data-analysis-with-python-summer-2019/part02-e14_vector_lengths/src/vector_lengths.py
@@ -4,10 +4,12 @@ import numpy as np
 #import scipy.linalg
 
 def vector_lengths(a):
-    return np.array([])
+    A = np.sqrt(np.sum(a**2, axis=1))
+    return np.array(A)
 
 def main():
-    pass
+    a = np.random.randint(10, size=(4,5))
+    print(vector_lengths(a))
 
 if __name__ == "__main__":
     main()
