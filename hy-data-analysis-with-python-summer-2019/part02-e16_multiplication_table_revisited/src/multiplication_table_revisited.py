8c61c7241862b0da6d7bb7c5733e304057b18b43
Miska Noponen <miska.noponen@gmail.com>
2020-01-28 21:14:00 +0200

    completed exercises part 2 and downloaded part 3 exercises

--- hy-data-analysis-with-python-summer-2019/part02-e16_multiplication_table_revisited/src/multiplication_table_revisited.py
+++ hy-data-analysis-with-python-summer-2019/part02-e16_multiplication_table_revisited/src/multiplication_table_revisited.py
@@ -3,10 +3,12 @@
 import numpy as np
 
 def multiplication_table(n):
-    return np.array([])
+    x = np.arange(n)
+    y = np.arange(n).reshape(n, 1)
+    return x*y
 
 def main():
-    print(multiplication_table(4))
+    print(multiplication_table(10))
 
 if __name__ == "__main__":
     main()
