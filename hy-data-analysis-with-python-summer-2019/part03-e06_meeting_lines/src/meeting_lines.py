c212f636fca288f6ba950d42a0c2f8521a289a07
Miska Noponen <miska.noponen@gmail.com>
2020-01-31 23:14:10 +0200

    completed part 3 exercise 6

--- hy-data-analysis-with-python-summer-2019/part03-e06_meeting_lines/src/meeting_lines.py
+++ hy-data-analysis-with-python-summer-2019/part03-e06_meeting_lines/src/meeting_lines.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 
 import numpy as np
+from scipy import linalg
 
 def meeting_lines(a1, b1, a2, b2):
-    return []
+    #x = linalg.solve([a1-a2],[b2-b1])[0]
+    x = linalg.solve([[a1, -1],[a2, -1]],[-b1,-b2])
+    return [x[0],x[1]]
 
 def main():
     a1=1
     b1=4
     a2=3
     b2=2
-
     x, y  = meeting_lines(a1, b1, a2, b2)
     print(f"Lines meet at x={x} and y={y}")
 
