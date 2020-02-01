eca35007f7ed4b27908b6dd053a5a44816f84db7
Miska Noponen <miska.noponen@gmail.com>
2020-02-01 15:32:21 +0200

    Completed part 3 exercise 10

--- hy-data-analysis-with-python-summer-2019/part03-e09_multiple_graphs/src/multiple_graphs.py
+++ hy-data-analysis-with-python-summer-2019/part03-e09_multiple_graphs/src/multiple_graphs.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    pass
+    a = np.array([2,4,6,7])
+    b = np.array([4,3,5,1])
+    c = np.array([1,2,3,4])
+    d = np.array([4,2,3,1])
+    plt.plot(a,b)
+    plt.plot(c,d)
+    plt.title("Strange snakes seen from above")
+    plt.xlabel("other fence wall")
+    plt.ylabel("fence wall")
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
