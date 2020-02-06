fd4a745e1a71633948c7ae35c343759c62edd040
Miska Noponen <miska.noponen@gmail.com>
2020-02-06 21:44:24 +0200

    completed part 3. Final exercise was 15

--- hy-data-analysis-with-python-summer-2019/part03-e15_inverse_series/src/inverse_series.py
+++ hy-data-analysis-with-python-summer-2019/part03-e15_inverse_series/src/inverse_series.py
@@ -3,10 +3,13 @@
 import pandas as pd
 
 def inverse_series(s):
-    return pd.Series()
+    return pd.Series(s.index, index=s.values)
 
 def main():
-    return
+    s1 = pd.Series([1,2,3,3], index=list("abcd"))
+    s_inv =inverse_series(s1)
+    print(s_inv)
+    print(s_inv[3])
 
 if __name__ == "__main__":
     main()
