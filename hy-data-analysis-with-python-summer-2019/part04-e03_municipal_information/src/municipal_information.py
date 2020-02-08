16394cc5a5de27bb01ef5ae58dc61c7d35bd823b
Miska Noponen <miska.noponen@gmail.com>
2020-02-08 17:30:32 +0200

    Completed part 4 exercise 3

--- hy-data-analysis-with-python-summer-2019/part04-e03_municipal_information/src/municipal_information.py
+++ hy-data-analysis-with-python-summer-2019/part04-e03_municipal_information/src/municipal_information.py
@@ -3,7 +3,12 @@
 import pandas as pd
 
 def main():
-    return
+    df = pd.read_csv("src/municipal.tsv", sep="\t")
+    rows, columns = df.shape
+    print(f"Shape: {rows},{columns}")
+    print("Columns:")
+    for i in df.columns:
+        print(i)
 
 
 if __name__ == "__main__":
