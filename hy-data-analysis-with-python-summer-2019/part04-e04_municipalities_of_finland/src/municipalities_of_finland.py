579b0567e9c3e8d1928b5c69cabbd57f04b19397
Miska Noponen <miska.noponen@gmail.com>
2020-02-09 16:00:39 +0200

    completed part 4 exercise 4

--- hy-data-analysis-with-python-summer-2019/part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
+++ hy-data-analysis-with-python-summer-2019/part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
@@ -3,10 +3,13 @@
 import pandas as pd
 
 def municipalities_of_finland():
-    return None
-    
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col="Region 2018")
+    df.drop('WHOLE COUNTRY', inplace=True)
+    df = df[: df.index.get_loc('Äänekoski')+1]
+    return df
+
 def main():
-    return
-    
+    print(municipalities_of_finland())
+
 if __name__ == "__main__":
     main()
