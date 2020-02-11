c24ac7b244e0c50b8fa3369a040fcec966c0498b
Miska Noponen <miska.noponen@gmail.com>
2020-02-11 21:28:44 +0200

    completed part 4 exercise 14

--- hy-data-analysis-with-python-summer-2019/part04-e14_special_missing_values/src/special_missing_values.py
+++ hy-data-analysis-with-python-summer-2019/part04-e14_special_missing_values/src/special_missing_values.py
@@ -4,10 +4,13 @@ import pandas as pd
 import numpy as np
 
 def special_missing_values():
-    return None
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df = df.replace(["Re", "New"], np.nan)
+    df = df.astype({'LW':'float'})
+    return df[df["Pos"] > df["LW"]]
 
 def main():
-    return
+    print(special_missing_values())
 
 if __name__ == "__main__":
     main()
