a373c53686d947b977ad19307872d8393df495ba
Miska Noponen <miska.noponen@gmail.com>
2020-02-11 19:34:21 +0200

    completed part 4 exercise 6

--- hy-data-analysis-with-python-summer-2019/part04-e06_growing_municipalities/src/growing_municipalities.py
+++ hy-data-analysis-with-python-summer-2019/part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -3,10 +3,14 @@
 import pandas as pd
 
 def growing_municipalities(df):
-    0.0
+    growers = df[df["Population change from the previous year, %"]>0]
+    ratio = growers.shape[0] / df.shape[0]
+    return ratio
 
 def main():
-    return
+    df = pd.read_csv("src/municipal.tsv", sep="\t")
+    ratio = growing_municipalities(df[:100])
+    print("Proportion of growing municipalities: {:.1f}%".format(ratio*100))
 
 if __name__ == "__main__":
     main()
