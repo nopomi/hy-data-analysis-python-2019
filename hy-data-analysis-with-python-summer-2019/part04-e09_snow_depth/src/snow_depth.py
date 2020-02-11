d417a3729b8d8d1045bbb063bc141ea5922b6191
Miska Noponen <miska.noponen@gmail.com>
2020-02-11 20:08:24 +0200

    completed part 4 exercise 9

--- hy-data-analysis-with-python-summer-2019/part04-e09_snow_depth/src/snow_depth.py
+++ hy-data-analysis-with-python-summer-2019/part04-e09_snow_depth/src/snow_depth.py
@@ -3,10 +3,12 @@
 import pandas as pd
 
 def snow_depth():
-    return 0.0
+    wh = pd.read_csv("src/kumpula-weather-2017.csv")
+    max = wh["Snow depth (cm)"].max()
+    return max
 
 def main():
-    return
+    print("Max snow depth: " + str(snow_depth()))
 
 if __name__ == "__main__":
     main()
