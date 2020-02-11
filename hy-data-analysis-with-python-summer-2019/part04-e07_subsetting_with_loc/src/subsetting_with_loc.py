eab799a92bbbc87820d694d38bc1a8c1c27c3b15
Miska Noponen <miska.noponen@gmail.com>
2020-02-11 19:46:44 +0200

    completed part 4 exercise 7

--- hy-data-analysis-with-python-summer-2019/part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
+++ hy-data-analysis-with-python-summer-2019/part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
@@ -3,10 +3,14 @@
 import pandas as pd
 
 def subsetting_with_loc():
-    return None
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    filtered = df.loc["Akaa":"Äänekoski", ["Population", "Share of Swedish-speakers of the population, %", "Share of foreign citizens of the population, %"]]
+    return filtered
 
 def main():
-    return
+    df = subsetting_with_loc()
+    print(df.shape)
+    print(df)
 
 if __name__ == "__main__":
     main()
