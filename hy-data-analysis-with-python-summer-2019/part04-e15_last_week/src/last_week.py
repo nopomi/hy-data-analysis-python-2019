3eb7609c27a907a620fb9ee2909bba0ce485e6a6
Miska Noponen <miska.noponen@gmail.com>
2020-02-16 17:02:57 +0200

    completed part 4 exercise 15

--- hy-data-analysis-with-python-summer-2019/part04-e15_last_week/src/last_week.py
+++ hy-data-analysis-with-python-summer-2019/part04-e15_last_week/src/last_week.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 
 def last_week():
-    return pd.DataFrame()
+    current = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    lw = current.copy()
+    lw["LW"] = lw["LW"].replace(["Re", "New"], np.nan)
+    lw = lw.astype({'LW': 'float'})
+    lw["Peak Pos"].where(lambda x: (lw["Peak Pos"] != lw["Pos"]) | (lw["Peak Pos"] == lw["LW"]), np.nan, inplace=True)
+    lw.drop(lw[lw["LW"].isna()].index, inplace=True)
+    lw["Pos"] = lw["LW"]
+    lw = lw.astype({'Pos':'int64'})
+    missing = pd.DataFrame([i for i in range(1,41) if i not in lw["Pos"].values], columns=["Pos"])
+    lw = lw.append(missing, sort=False)
+    lw["WoC"] = lw["WoC"]-1
+    lw["LW"] = np.nan
+    lw.sort_values(by = 'Pos', inplace=True)
+    lw.reset_index(inplace=True, drop=True)
+    return lw
 
 def main():
     df = last_week()
