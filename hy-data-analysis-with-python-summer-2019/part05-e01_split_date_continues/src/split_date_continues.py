79f3e2a28970733facb487570592e28039fe0449
Miska Noponen <miska.noponen@gmail.com>
2020-04-13 21:05:48 +0300

    Completed part 5 exercises 1 and 2

--- hy-data-analysis-with-python-summer-2019/part05-e01_split_date_continues/src/split_date_continues.py
+++ hy-data-analysis-with-python-summer-2019/part05-e01_split_date_continues/src/split_date_continues.py
@@ -2,22 +2,24 @@
 
 import pandas as pd
 
-
 def split_date(df):
-    d = df["Päivämäärä"].str.split(expand=True)
-    d.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
-
-    hourmin = d["Hour"].str.split(":", expand=True)
-    d["Hour"] = hourmin.iloc[:, 0]
-
-    d["Weekday"] = d["Weekday"].map(days)
-    d["Month"] = d["Month"].map(months)
-    
-    d = d.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
-    return d
+    df = df["Päivämäärä"].str.split(expand=True)
+    df = df.dropna(how="all")
+    df.columns=["Weekday", "Day", "Month", "Year", "Hour"]
+    df["Hour"] = [i.split(":")[0] for i in df["Hour"]]
+    df = df.astype({"Day":'int', "Year":'int', "Hour":'int'})
+    df["Weekday"].replace(["ma", "ti", "ke", "to", "pe", "la", "su"], ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"], inplace=True)
+    df["Month"] = df["Month"].map({"tammi":1, "helmi":2, "maalis":3, "huhti":4, "touko":5, "kesä":6, "heinä":7, "elo":8, "syys":9, "loka":10, "marras":11, "joulu":12})
+    df = df.astype({"Day":'int', "Year":'int', "Hour":'int', "Weekday":'object'})
+    return df
 
 def split_date_continues():
-    return None
+    df = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=';')
+    df = df.dropna(how='all', axis=0).dropna(how='all', axis=1)
+    dates = split_date(df)
+    df.drop('Päivämäärä', axis=1, inplace=True)
+    df = pd.concat([dates, df], axis=1)
+    return df
 
 def main():
     df = split_date_continues()
--- hy-data-analysis-with-python-summer-2019/part05-e02_cycling_weather/src/cycling_weather.py
+++ hy-data-analysis-with-python-summer-2019/part05-e02_cycling_weather/src/cycling_weather.py
@@ -2,12 +2,36 @@
 
 import pandas as pd
 
+days = dict(zip("ma ti ke to pe la su".split(), "Mon Tue Wed Thu Fri Sat Sun".split()))
+months = dict(zip("tammi helmi maalis huhti touko kesä heinä elo syys loka marras joulu".split(), range(1, 13)))
+
+def split_date(df):
+    d = df["Päivämäärä"].str.split(expand=True)
+    d.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+    hourmin = d["Hour"].str.split(":", expand=True)
+    d["Hour"] = hourmin.iloc[:, 0]
+    d["Weekday"] = d["Weekday"].map(days)
+    d["Month"] = d["Month"].map(months)
+    d = d.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+    return d
+
 
 def cycling_weather():
-    return None
+    pm = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=';')
+    kw = pd.read_csv('src/kumpula-weather-2017.csv', sep=',')
+    pm = pm.dropna(axis=0, how="all").dropna(axis=1, how="all")
+    d = split_date(pm)
+    pm = pm.drop("Päivämäärä", axis=1)
+    pm = pd.concat([d, pm], axis=1)
+    merged = pd.merge(pm, kw, left_on=['Year', 'Month', 'Day'], right_on=['Year', 'm', 'd'])
+    merged.drop(['m', 'd', 'Time', 'Time zone'], axis=1, inplace=True)
+    return merged
 
 def main():
-    return
+    df = cycling_weather()
+    print(df.shape)
+    print(df.keys())
+    print(df.head())
 
 if __name__ == "__main__":
     main()
