3c7ffe1aa9342ab9e7ac1b6bd4d81367ad35a95e
Miska Noponen <miska.noponen@gmail.com>
2020-04-13 22:45:12 +0300

    completed part 5 exercise 5

--- hy-data-analysis-with-python-summer-2019/part05-e05_best_record_company/src/best_record_company.py
+++ hy-data-analysis-with-python-summer-2019/part05-e05_best_record_company/src/best_record_company.py
@@ -3,11 +3,13 @@
 import pandas as pd
 
 def best_record_company():
-    return None
+    df = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep="\t")
+    best_of = df.groupby("Publisher").sum()['WoC'].idxmax()
+    return df[df['Publisher'] == best_of]
 
 def main():
-    return
-    
+    df = best_record_company()
+    print(df)
 
 if __name__ == "__main__":
     main()
