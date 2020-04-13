b88d34165d8fe4ad6fcd42cb4e19018d7c6d7f81
Miska Noponen <miska.noponen@gmail.com>
2020-04-13 21:41:16 +0300

    completed part 5 exercise 3

--- hy-data-analysis-with-python-summer-2019/part05-e03_top_bands/src/top_bands.py
+++ hy-data-analysis-with-python-summer-2019/part05-e03_top_bands/src/top_bands.py
@@ -3,10 +3,17 @@
 import pandas as pd
 
 def top_bands():
-    return pd.DataFrame()
+    bands = pd.read_csv('src/bands.tsv', sep='\t')
+    top_list = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep='\t')
+    bands['Band'] = bands['Band'].str.upper()
+    df = pd.merge(bands, top_list, left_on='Band', right_on='Artist')
+    return df
 
 def main():
-    return
+    df = top_bands()
+    print(df.shape)
+    print(df.keys())
+    print(df.head())
 
 if __name__ == "__main__":
     main()
