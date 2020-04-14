afb611cc68d57b866096544dc19017205737eaac
Miska Noponen <miska.noponen@gmail.com>
2020-04-14 21:35:05 +0300

    completed part 5 exercise 6

--- hy-data-analysis-with-python-summer-2019/part05-e06_suicide_fractions/src/suicide_fractions.py
+++ hy-data-analysis-with-python-summer-2019/part05-e06_suicide_fractions/src/suicide_fractions.py
@@ -3,10 +3,17 @@
 import pandas as pd
 
 def suicide_fractions():
-    return pd.Series()
+    fractions = pd.read_csv('src/who_suicide_statistics.csv')
+    #fractions = df.groupby('country').apply(lambda df.mean() : df['suicides_no']/df['population'])
+    fractions['fraction'] = fractions['suicides_no'] / fractions['population']
+    fractions = fractions.groupby('country').mean()
+    return fractions['fraction']
 
 def main():
-    return
+    df = suicide_fractions()
+    print(df.shape)
+    print(df.keys())
+    print(df.head())
 
 if __name__ == "__main__":
     main()
