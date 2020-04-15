6df9ba4b687c4f20efef9291aa804dd0eb1033a2
Miska Noponen <miska.noponen@gmail.com>
2020-04-15 22:49:06 +0300

    completed part 5 exercise 8

--- hy-data-analysis-with-python-summer-2019/part05-e08_bicycle_timeseries/src/bicycle_timeseries.py
+++ hy-data-analysis-with-python-summer-2019/part05-e08_bicycle_timeseries/src/bicycle_timeseries.py
@@ -2,13 +2,27 @@
 
 import pandas as pd
 
-
 def bicycle_timeseries():
-    return pd.DataFrame()
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df2 = df["Päivämäärä"].str.split(expand=True)
+    df2.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+    hourmin = df2["Hour"].str.split(":", expand=True)
+    df2["Hour"] = hourmin.iloc[:, 0]
+    df = pd.merge(df, df2, left_index=True, right_index=True)
+    df = df.dropna(how='all')
+    df["Month"] = df["Month"].map({"tammi":1, "helmi":2, "maalis":3, "huhti":4, "touko":5, "kesä":6, "heinä":7, "elo":8, "syys":9, "loka":10, "marras":11, "joulu":12})
+    df.astype({'Day':'int', 'Month':'int', 'Year':'int', 'Hour':'int'})
+    df["Datetime"] = pd.to_datetime(df[["Year", "Month", "Day", "Hour"]])
+    df = df.drop(columns=["Päivämäärä", "Unnamed: 21", "Year", "Month", "Day", "Hour", "Weekday"])
+    df = df.set_index("Datetime")
+    return df
 
 
 def main():
-    return None
+    df = bicycle_timeseries()
+    print(df.shape)
+    print(df.head())
+    print(df.keys())
 
 if __name__ == "__main__":
     main()
