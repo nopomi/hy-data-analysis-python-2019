bc548f199aa27c77563aac1d52c1d13a83381158
Miska Noponen <miska.noponen@gmail.com>
2020-05-24 19:21:43 +0300

    Completed part 5 exercise 10. First ML model, yay!

--- hy-data-analysis-with-python-summer-2019/part05-e10_linear_regression/src/linear_regression.py
+++ hy-data-analysis-with-python-summer-2019/part05-e10_linear_regression/src/linear_regression.py
@@ -5,9 +5,23 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 def fit_line(x, y):
-    return 0, 0
-    
+    model = LinearRegression()
+    model.fit(x[:,np.newaxis], y)
+    return model.coef_[0], model.intercept_
+
 def main():
-    
+    n = 100
+    x = np.linspace(0, 10, n)
+    y = x*1.5 + 2 + 1*np.random.randn(n)
+    slope, intercept = fit_line(x,y)
+    print('Slope: ', slope)
+    print('Intercept: ', intercept)
+    plt.plot(x, y, 'ro')
+    xf = np.linspace(0, 10, 50)
+    yf = xf*slope + intercept
+    plt.plot(xf, yf, color="black")
+    plt.show()
+
+
 if __name__ == "__main__":
     main()
