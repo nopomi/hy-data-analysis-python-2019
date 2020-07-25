dcd771b78b40669ebc1309d671e3f36e22eb86bd
Miska Noponen <miska.noponen@gmail.com>
2020-07-25 20:26:28 +0300

    in progress on part 6 exercise 4

--- hy-data-analysis-with-python-2020/part06-e04_spam_detection/src/spam_detection.py
+++ hy-data-analysis-with-python-2020/part06-e04_spam_detection/src/spam_detection.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python3
+import gzip
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
+
+def read_files(fraction):
+    ham_lines = int(len(gzip.open("src/spam.txt.gz").readlines()) * fraction)
+    spam_lines = int(len(gzip.open("src/spam.txt.gz").readlines()) * fraction)
+
+    ham = np.array(ham_lines)
+    with gzip.open("src/ham.txt.gz") as f:
+        for i, line in enumerate(f):
+            if i > ham_lines: break
+            if line == "": continue
+            ham = np.append(ham,line)
+
+    spam = np.array(spam_lines)
+    with gzip.open("src/spam.txt.gz") as f:
+        for i, line in enumerate(f):
+            if i > spam_lines: break
+            if line== "": continue
+            spam = np.append(spam, line)
+
+    return ham, spam
 
 def spam_detection(random_state=0, fraction=1.0):
+    ham, spam = read_files(fraction)
+    vectorizer = CountVectorizer()
+    X_ham = vectorizer.fit_transform(ham)
+    X_spam = vectorizer.fit_transform(spam)
+    print(X_ham.dtype)
     return 0.0, 0, 0
 
 def main():
