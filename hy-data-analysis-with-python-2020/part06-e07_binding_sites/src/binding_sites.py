3498749cc70c904de1b797fab0c3f60699698147
Miska Noponen <miska.noponen@gmail.com>
2020-07-28 22:20:25 +0300

    Completed part 6 exercise 7

--- hy-data-analysis-with-python-2020/part06-e07_binding_sites/src/binding_sites.py
+++ hy-data-analysis-with-python-2020/part06-e07_binding_sites/src/binding_sites.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+import scipy
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import pairwise_distances
@@ -13,11 +14,22 @@ sns.set(color_codes=True)
 import scipy.spatial as sp
 import scipy.cluster.hierarchy as hc
 
+def find_permutation(n_clusters, real_labels, labels):
+    permutation=[]
+    for i in range(n_clusters):
+        idx = labels == i
+        # Choose the most common label among data points in the cluster
+        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        permutation.append(new_label)
+    return permutation
+
 def toint(x):
-    return 0
+    return 'ACGT'.find(x)
 
 def get_features_and_labels(filename):
-    return (np.array([[]]), np.array([]))
+    df = pd.read_csv(filename, sep="\t")
+    X = [[toint(c) for c in s] for s in df["X"]]
+    return (np.array(X), np.array(df["y"]))
 
 def plot(distances, method='average', affinity='euclidean'):
     mylinkage = hc.linkage(sp.distance.squareform(distances), method=method)
@@ -26,10 +38,26 @@ def plot(distances, method='average', affinity='euclidean'):
     plt.show()
 
 def cluster_euclidean(filename):
-    return 0.0
+    X, y = get_features_and_labels(filename)
+    model = AgglomerativeClustering(linkage="average", affinity="euclidean")
+    model.fit(X)
+    permutation = find_permutation(2, y, model.labels_)
+    new_labels = [permutation[label] for label in model.labels_]
+    score = accuracy_score(y, new_labels)
+    distances=pairwise_distances(X, metric="euclidean")
+    #plot(distances)
+    return score
 
 def cluster_hamming(filename):
-    return 0.0
+    X, y = get_features_and_labels(filename)
+    distances = pairwise_distances(X, metric="hamming")
+    model = AgglomerativeClustering(affinity="precomputed", linkage="average")
+    model.fit_predict(distances)
+    permutation = find_permutation(2, y, model.labels_)
+    new_labels = [permutation[label] for label in model.labels_]
+    score = accuracy_score(y, new_labels)
+    #plot(distances, method="average", affinity="hamming")
+    return score
 
 
 def main():
