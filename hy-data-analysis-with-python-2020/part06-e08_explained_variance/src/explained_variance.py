8e92b644cbc15c2407c020aee390a3078c580b9c
Miska Noponen <miska.noponen@gmail.com>
2020-08-02 18:56:52 +0300

    completed part 6 exercise 8 (last exercise). Downloaded project exercises.

--- hy-data-analysis-with-python-2020/part06-e08_explained_variance/src/explained_variance.py
+++ hy-data-analysis-with-python-2020/part06-e08_explained_variance/src/explained_variance.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 def explained_variance():
-    return [], []
+    df = pd.read_csv("src/data.tsv", sep="\t")
+    model = PCA()
+    model.fit(df)
+    return df.var(axis=0), model.explained_variance_
 
 def main():
     v, ev = explained_variance()
-    print(sum(v), sum(ev))
+    print("The variances are: ", end="")
+    for i in v:
+        print(f"{i:.3f}", end=" ")
+    print("\n")
+    print("The explained variances after PCA are: ", end="")
+    for j in ev:
+        print(f"{j:.3f}", end=" ")
+    print("\n")
+    plt.plot(np.arange(1,11), np.cumsum(ev))
+    plt.show()
 
 if __name__ == "__main__":
     main()
